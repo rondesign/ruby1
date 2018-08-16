df4e74d4e2d3a1b9bc9ec89a0ba0a40721efc3be
rondesign <ronlawson@gmail.com>
2018-08-16 12:19:26 -0700

    add methods

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,18 +1,12 @@
-name1 = 'larry'
-name2 = 'curly'
-name3 = 'moe'
-name4 = 'shemp'
-health1 = 30
-health2 = 60
-health3 = 100
-health4 = 90
-current_time = Time.new
+def time
+  Time.new.strftime('%T')
+end
 
-puts "#{name1.capitalize} has a health of #{health1}."
-puts "#{name2.upcase} has a health of #{health2}."
-puts "#{name3.capitalize} has a health of #{health3}.".center(50, '*')
-puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
+def say_hello(name, health = 100)
+  "I'm #{name.capitalize} with a health of #{health} as of #{time}"
+end
 
-puts "The game started on #{current_time.strftime('%A %m/%d/%Y at %I:%M%p')}"
-
-puts "Players:\n\t#{name1.capitalize}\n\t#{name2}\n\t#{name3}"
+puts say_hello('larry', 60)
+puts say_hello('curly', 125)
+puts say_hello('moe')
+puts say_hello('shemp', 90)
